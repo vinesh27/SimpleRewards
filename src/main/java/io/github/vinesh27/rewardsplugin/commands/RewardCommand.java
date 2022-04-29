e822714dfc43926a7477c502f9afe4fcbcb54ace
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-29 14:34:34 +0530

    Added RewardCommand, Opens RewardGUI

---
+++ src/main/java/io/github/vinesh27/rewardsplugin/commands/RewardCommand.java
@@ -0,0 +1,15 @@
+package io.github.vinesh27.rewardsplugin.commands;
+
+import io.github.vinesh27.rewardsplugin.gui.RewardGUI;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RewardCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        new RewardGUI((Player) sender);
+        return true;
+    }
+}
