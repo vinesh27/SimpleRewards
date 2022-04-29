aeffa914ff25a356f23b3eb6c7e7a23308b018b9
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-29 15:16:30 +0530

    Registered Events & Commands

--- src/main/java/io/github/vinesh27/rewardsplugin/RewardsPlugin.java
+++ src/main/java/io/github/vinesh27/rewardsplugin/RewardsPlugin.java
@@ -1,5 +1,9 @@
 package io.github.vinesh27.rewardsplugin;
 
+import io.github.vinesh27.rewardsplugin.commands.RewardCommand;
+import io.github.vinesh27.rewardsplugin.gui.RewardGUI;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RewardsPlugin extends JavaPlugin {
@@ -9,7 +13,11 @@ public final class RewardsPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Loading " + ChatColor.GOLD + "RewardsPlugin...");
+        instance = this;
+        this.getCommand("rewards").setExecutor(new RewardCommand());
+        this.getServer().getPluginManager().registerEvents(new RewardGUI(), this);
+        Bukkit.getConsoleSender().sendMessage(ChatColor.GREEN + "Loaded " + ChatColor.GOLD + "RewardsPlugin");
     }
     
     @Override
